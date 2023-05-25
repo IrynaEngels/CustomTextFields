086fe7d13fe271574aa3d60879b5d8656806b90b
IrynaEngels <engelsiryna@gmail.com>
2023-05-25 14:54:23 +0300

    crashlytics added

--- app/src/main/java/com/irynaengels/dymanictextfieldproperties/MainActivity.kt
+++ app/src/main/java/com/irynaengels/dymanictextfieldproperties/MainActivity.kt
@@ -21,6 +21,7 @@ import com.irynaengels.dymanictextfieldproperties.ui.MainNavigation
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             DymanicTextFieldPropertiesTheme {
                 Surface(
